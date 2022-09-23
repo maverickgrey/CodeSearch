af513dcfd2188f758ee64c25771190bf5abfcdbc
ziqiancheng <ziqiancheng0606@qq.com>
2022-09-23 19:31:03 +0800

    增加了分类器训练数据集

--- dataset.py
+++ dataset.py
@@ -2,9 +2,11 @@ from torch.utils.data import Dataset
 import json
 import torch
 from utils import convert_examples_to_features
-import config_class
+from config_class import Config
 
 
+config = Config()
+# 训练encoder的数据集与一般使用时的数据集
 class CodeSearchDataset(Dataset):
     def __init__(self,tokenizer,mode='train'):
         self.mode = mode
@@ -21,7 +23,7 @@ class CodeSearchDataset(Dataset):
     def load_example(self,mode):
         examples = []
         if mode == 'test':
-            test_path = config_class.data_path + "/java_test_0.jsonl"
+            test_path = config.data_path + "/java_test_0.jsonl"
             with open(test_path,'r',encoding='utf-8') as f:
                 num = 0
                 for line in f.readlines():
@@ -33,7 +35,7 @@ class CodeSearchDataset(Dataset):
                         break
                 return examples
         elif mode == 'eval':
-            eval_path = config_class.data_path + "/java_valid_0.jsonl"
+            eval_path = config.data_path + "/java_valid_0.jsonl"
             with open(eval_path,'r',encoding='utf-8') as f:
                 num = 0
                 for line in f.readlines():
@@ -46,7 +48,7 @@ class CodeSearchDataset(Dataset):
             return examples
         else:
             num = 0
-            train_path = config_class.data_path +"/java_train_"
+            train_path = config.data_path +"/java_train_"
             for i in range(1):
                 postfix = str(i)+".jsonl"
                 train = train_path + postfix
@@ -59,3 +61,34 @@ class CodeSearchDataset(Dataset):
                         # if num>9999:
                         #   break
             return examples
+
+# 专门用来训练Classifier的数据集
+class ClassifierDataset(Dataset):
+    def __init__(self,tokenizer,mode='train'):
+        self.tokenizer= tokenizer
+        self.mode = mode
+        self.data = self.load_examples(self.mode)
+    
+    def load_examples(self,mode):
+        if mode == 'eval':
+            pass
+        elif mode == 'test':
+            pass
+        else:
+            examples = []
+            train_path = config.data_path+"/classifier/ctrain_0.jsonl"
+            with open(train_path,'r') as f:
+                for line in f.readlines():
+                    js = json.loads(line)
+                    example = convert_examples_to_features(js,config.tokenizer,-1,True)
+                    examples.append(example)
+            return examples
+
+    def __getitem__(self, index):
+        pl_ids = self.data[index].pl_ids
+        nl_ids = self.data[index].nl_ids
+        label = self.data[index].label
+        return (torch.tensor(pl_ids),torch.tensor(nl_ids),torch.tensor(label))
+    
+    def __len__(self):
+        return len(self.data)
