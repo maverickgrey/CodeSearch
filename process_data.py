6ddc0eec9dde7ed7986409a652dc2ea942570de1
maverickgrey <1109342846@qq.com>
2022-10-26 18:44:35 +0800

    add pipeline and others

--- process_data.py
+++ process_data.py
@@ -3,6 +3,7 @@ import random
 import os
 import re
 import string
+from config_class import Config
 
 """
 本文件对CodeSearchNet的源数据进行一定的处理：
@@ -12,11 +13,13 @@ import string
 """
 
 class TrainData:
-    def __init__(self,code_tokens,nl_tokens,code,docstring) -> None:
+    def __init__(self,code_tokens,nl_tokens,code,docstring,func_name,repo) -> None:
         self.code_tokens = code_tokens
         self.nl_tokens = nl_tokens
         self.code = code
         self.docstring = docstring
+        self.func_name = func_name
+        self.repo = repo
 
 class TargetData:
     def __init__(self,label,code_tokens,nl_tokens,code,docstring) -> None:
@@ -33,6 +36,7 @@ class DataFilterer:
             self.data = self.load_data(self.data_path)
         else:
             self.data = data
+        self.filter_table = ['<p','()','=','<br','>','/','<ul','<li','<ol','{','{@','}','\n','(',')','*','@','--']
 
     def filter(self,data):
         result = []
@@ -44,6 +48,7 @@ class DataFilterer:
             elif self.has_java(data[i]):
                 continue
             else:
+                data[i].nl_tokens = self.detach_punctuation(data[i])
                 data[i].nl_tokens = self.too_long(data[i])
                 result.append(data[i])
         return result
@@ -53,7 +58,7 @@ class DataFilterer:
         with open(data_path,'r') as f:
             for line in f.readlines():
                 js = json.loads(line)
-                data.append(TrainData(js['code_tokens'],js['docstring_tokens'],js['code'],js['docstring']))
+                data.append(TrainData(js['code_tokens'],js['docstring_tokens'],js['code'],js['docstring'],js['func_name'],js['repo']))
         return data
     
     # 一些过滤规则，True则应该过滤，False则不应该过滤
@@ -65,7 +70,7 @@ class DataFilterer:
         for token in nl_tokens:
             if token not in punc:
                 length += 1
-        return True if length<=3 else False
+        return True if length<=4 else False
     
     # 第二条过滤规则：将中文的NL过滤出去，因为它们在数据集中是以unicode的形式存储的，这在模型中无法提供有用的语义信息
     def has_chinese(self,example):
@@ -95,9 +100,22 @@ class DataFilterer:
         if '@' in nl[0]:
             return True
         return False
+
+    # 第五条规则：将docstring_tokens中含有黑名单中符号的部分拿掉
+    def detach_punctuation(self,example):
+        nl_tokens = []
+        for i in range(len(example.nl_tokens)):
+            flag = False
+            for p in self.filter_table:
+                if p in example.nl_tokens[i]:
+                    flag = True
+                    break
+            if flag == False:
+                nl_tokens.append(example.nl_tokens[i])
+        return nl_tokens
     
 
-# 用来处理多文件数据集
+# 用多文件数据构建classifier数据集
 def process_files(filter=True,shuffle = False):
     path_prefix = "./CodeSearchNet/origin_data/java_train_"
     data_filter = None
@@ -131,7 +149,7 @@ def process_files(filter=True,shuffle = False):
                 js['label'] = example.label
                 ft.write(json.dumps(js)+"\n")
 
-# 处理单文件数据
+# 用单文件数据构建classifier数据集
 def process_a_file(data_path,out_path,filter=False,shuffle=False):
     data = []
     with open(data_path,'r') as f:
@@ -155,21 +173,49 @@ def process_a_file(data_path,out_path,filter=False,shuffle=False):
             ft.write(json.dumps(js)+"\n")
 
 
-# 构建正样本和负样本
+# 构建分类的正样本和负样本
 def build_examples(data,data_filter=None,shuffle=False):
     result = []
-    for i in range(len(data)):
-        # 正样本
-        positive = TargetData(1,data[i].code_tokens,data[i].nl_tokens,data[i].code,data[i].docstring)
-
-        # 负样本*1
-        r1 = random.randint(0,len(data)-1)
-        while(r1 == i):
-            r1 = random.randint(0,len(data)-1)
-        negative1 = TargetData(0,data[r1].code_tokens,data[i].nl_tokens,data[r1].code,data[i].docstring)
-
-        result.append(positive)
-        result.append(negative1)
+    if len(data)>=2 and len(data)<4:
+    #构建两个样本，当当前data长度在2-4之间时
+        for i in range(len(data)):
+            scripts = [i]
+            sample = TargetData(1,data[i].code_tokens,data[i].nl_tokens,data[i].code,data[i].docstring)
+            result.append(sample)
+            k=0
+            while k<1:
+                r = random.randint(0,len(data)-1)
+                while (r in scripts):
+                    r = random.randint(0,len(data)-1)
+                scripts.append(r)
+                if data[r].func_name == data[i].func_name:
+                    sample = TargetData(1,data[r].code_tokens,data[i].nl_tokens,data[r].code,data[i].docstring)
+                    result.append(sample)
+                    k += 1
+                else:
+                    sample = TargetData(0,data[r].code_tokens,data[i].nl_tokens,data[r].code,data[i].docstring)
+                    result.append(sample)
+                    k += 1
+    elif len(data)>=4:
+    #每条数据构建4个样本：当当前data长度大于等于4时
+        for i in range(len(data)):
+            scripts = [i]
+            sample = TargetData(1,data[i].code_tokens,data[i].nl_tokens,data[i].code,data[i].docstring)
+            result.append(sample)
+            k=0
+            while k<3:
+                r = random.randint(0,len(data)-1)
+                while (r in scripts):
+                    r = random.randint(0,len(data)-1)
+                scripts.append(r)
+                if data[r].func_name == data[i].func_name:
+                    sample = TargetData(1,data[r].code_tokens,data[i].nl_tokens,data[r].code,data[i].docstring)
+                    result.append(sample)
+                    k += 1
+                else:
+                    sample = TargetData(0,data[r].code_tokens,data[i].nl_tokens,data[r].code,data[i].docstring)
+                    result.append(sample)
+                    k += 1
     if shuffle:
         shuffle_data(result)
     if data_filter is not None:
@@ -212,18 +258,15 @@ def stat_nltokens(data):
                 f.write(json.dumps(js)+"\n")
     print(stat)
 
-def filter_encoder_data():
-    prefix = "./CodeSearchNet/origin_data/java_train_"
-    if not os.path.exists("CodeSearchNet/filted_data"):
-        os.mkdir("CodeSearchNet/filted_data")
-    for i in range(16):
-        train_path = prefix + str(i)+".jsonl"
-        out_path = "./CodeSearchNet/filted_data/java_train_f_"+str(i)+".jsonl"
-        data_filter = DataFilterer(train_path)
+def filter_encoder_data(data_path,out_path):
+    with open(data_path,'r') as f:
+        data_filter = DataFilterer(data_path)
         result = data_filter.filter(data_filter.data)
         with open(out_path,'w') as f:
             for example in result:
                 js = {}
+                js['repo'] = example.repo
+                js['func_name'] = example.func_name
                 js['docstring'] = example.docstring
                 js['docstring_tokens'] = example.nl_tokens
                 js['code'] = example.code
@@ -232,8 +275,8 @@ def filter_encoder_data():
 
 # 把所有的训练数据弄到一个文件里
 def converge():
-    prefix = "./CodeSearchNet/classifier/ctrain_"
-    out_path = "./CodeSearchNet/classifier/java_train_c.jsonl"
+    prefix = "./CodeSearchNet/classifier/java_train_scc"
+    out_path = "./CodeSearchNet/classifier/java_train_c3.jsonl"
     f = open(out_path,'a')
     for train_no in range(16):
         postfix = str(train_no)+".jsonl"
@@ -246,13 +289,113 @@ def converge():
                 read['docstring_tokens'] = js['docstring_tokens']
                 read['code'] = js['code']
                 read['code_tokens'] = js['code_tokens']
-                read['label'] = js['label']
                 f.write(json.dumps(read)+'\n')
     f.close()
     
+# 将训练集数据按照函数名分类，每个类别之间空一行
+def category_by_funcname(datapath,out_path):
+    out_file = open(out_path,'w')
+    current_name_prefix = None
+    with open(datapath,'r') as f:
+        line0 = f.readline()
+        js0 = json.loads(line0)
+        func_name0 = js0['func_name']
+        current_name_prefix = func_name0.split('.')[0]
+        out_file.write(line0)
+        for line in f.readlines():
+            js = json.loads(line)
+            func_name = js['func_name']
+            func_name = func_name.split('.')
+            name_prefix = func_name[0]
+            if name_prefix == current_name_prefix:
+                out_file.write(line)
+            else:
+                out_file.write("\n"+line)
+                current_name_prefix = name_prefix
+    out_file.close()
+
+# 用分类好的数据构建分类数据集
+def build_by_categorized(data_path,out_path):
+    out = open(out_path,'w')
+    result = []
+    with open(data_path,'r') as f:
+        data = []
+        unique_prefix_data = []
+        for line in f.readlines():
+            if line != "\n":
+                js = json.loads(line)
+                data.append(TrainData(js['code_tokens'],js['docstring_tokens'],js['code'],js['docstring'],js['func_name'],js['repo']))
+            else:
+                if len(data) == 1:
+                    unique_prefix_data.extend(data)
+                    data = []
+                else:
+                    result.extend(build_examples(data,shuffle=True))
+                    data=[]
+        result.extend(build_examples(unique_prefix_data,shuffle=True))
+    for r in result:
+        js = {}
+        js['docstring'] = r.docstring
+        js['docstring_tokens'] = r.nl_tokens
+        js['code'] = r.code
+        js['code_tokens'] = r.code_tokens
+        js['label'] = r.label
+        out.write(json.dumps(js)+"\n")
+    out.close()
+
+# 用func_name和repo来增强查询
+def strengthen_query(data):
+    for example in data:
+        nl_tokens = [example.repo.split('/'[-1])]
+        nl_tokens.extend(example.func_name.split('.'))
+        nl_tokens.extend(example.nl_tokens)
+        example.nl_tokens = nl_tokens
+
+def write_to_file(data,file,type='encoder'):
+    with open(file,'w') as f:
+        for example in data:
+            js = {}
+            if type == 'encoder':
+                js['repo'] = example.repo
+                js['func_name'] = example.func_name
+            js['docstring'] = example.docstring
+            js['docstring_tokens'] = example.docstring_tokens
+            js['code'] = example.code
+            js['code_tokens'] = example.code_tokens
+            f.write(json.dumps(js)+"\n")
+
+# 处理数据的pipeline:读入一个原始数据文件，分别输出一个encoder的文件以及一个classifier的文件
+# 处理流程包括了查询增强、过滤数据、按照func_name分类、构建classifier数据、将数据写入到输出文件等
+def pipeline(data_path,encoder_out_path,classifier_out_path):
+    # 读入数据
+    data = []
+    with open(data_path,'r') as f:
+        for line in f.readlines():
+            js = json.loads(line)
+            example = TrainData(js['code_tokens'],js['docstring_tokens'],js['code'],js['docstring'],js['func_name'],js['repo'])
+            data.append(example)
+    #查询增强
+    strengthen_query(data)
+    #过滤数据并写入文件
+    df = DataFilterer(data_path=None,data=data)
+    data = df.filter(df.data)
+    write_to_file(data,encoder_out_path,'encoder')
+
+    #按照func_name分类
+    temp_file = "./CodeSearchNet/classifier/temp.jsonl"
+    category_by_funcname(encoder_out_path,temp_file)
+    #构建classifier数据
+    build_by_categorized(temp_file,classifier_out_path)
+    os.remove(temp_file)
+
 
 if __name__ == "__main__":
     # train_path = "./CodeSearchNet/classifier/java_train_cl.jsonl"
     # out_path = "./CodeSearchNet/classifier/java_train_c.jsonl"
     # process_a_file(train_path,out_path,True,True)
-    converge()
+    # converge()
+    config = Config()
+    for i in range(16):
+        data_path = config.data_path+"/strengthened/java_train_s"+str(i)+".jsonl"
+        out_path = config.data_path+"/filtered_data/java_train_sf"+str(i)+".jsonl"
+        filter_encoder_data(data_path,out_path)
